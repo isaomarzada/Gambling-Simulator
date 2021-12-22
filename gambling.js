54a18cb27db3f34d9d77533e3f5c538dfbb51895
unknown <isaomarzada@yahoo.com>
2021-12-22 03:34:12 -0600

    added hovering feature and functionionality

--- gambling.js
+++ gambling.js
@@ -1,3 +1,14 @@
+var squareOne = document.getElementById('square-one');
+var squareTwo = document.getElementById('square-two');
+var squareThree = document.getElementById('square-three');
+var squareFour = document.getElementById('square-four');
+var squareFive = document.getElementById('square-five');
+var squareSix = document.getElementById('square-six');
+var squareSeven = document.getElementById('square-seven');
+var squareEight = document.getElementById('square-eight');
+var squareNine = document.getElementById('square-nine');
+var array = [squareOne, squareTwo, squareThree, squareFour, squareFive, squareSix, squareSeven, squareEight, squareNine];
+
 function enterAmount(){
   var gamblingAmountText = document.getElementById('amount-left');
   var gamblingAmount = document.getElementById('gambling-amount');
@@ -8,3 +19,26 @@ function enterAmount(){
 
   gamblingAmountText.innerHTML = gamblingAmount.value;
 }
+
+function bombSquare() {
+  var random = Math.floor(Math.random() * 9) + 1;
+  console.log(random);
+}
+
+function newRound() {
+  var isBombSquare;
+  var random = Math.floor(Math.random() * 9) + 1;
+  for (let i = 1; i <= 9; i++) {
+    array[i] = i;
+
+  }
+  for (let i = 1; i <= 9; i++) {
+    if (array[i] == random) {
+      array[i] = "bombSquare";
+    }
+
+  }
+
+}
+
+bombSquare();
