2bebc5da3208a6ba288871cfe0169099bc96f5b1
unknown <isaomarzada@yahoo.com>
2021-12-22 01:12:37 -0600

    user able to enter gambling start amount

--- gambling.js
+++ gambling.js
@@ -1,6 +1,10 @@
 function enterAmount(){
-  
+  var gamblingAmountText = document.getElementById('amount-left');
   var gamblingAmount = document.getElementById('gambling-amount');
   var formContent = document.getElementById('amount-input');
 
+  formContent.innerHTML = "";
+  console.log(gamblingAmount.value);
+
+  gamblingAmountText.innerHTML = gamblingAmount.value;
 }
