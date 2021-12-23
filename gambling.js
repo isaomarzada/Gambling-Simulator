ea491f5b354e9af146ea445fba12a364ecb1bb37
unknown <isaomarzada@yahoo.com>
2021-12-22 18:59:45 -0600

    bomb square feature added

--- gambling.js
+++ gambling.js
@@ -7,7 +7,11 @@ var squareSix = document.getElementById('square-six');
 var squareSeven = document.getElementById('square-seven');
 var squareEight = document.getElementById('square-eight');
 var squareNine = document.getElementById('square-nine');
+var gamblingAmountText = document.getElementById('amount-left');
+var gamblingAmount = document.getElementById('gambling-amount');
+gamblingAmount.value;
 var array = [squareOne, squareTwo, squareThree, squareFour, squareFive, squareSix, squareSeven, squareEight, squareNine];
+console.log(array[2]);
 
 function enterAmount(){
   var gamblingAmountText = document.getElementById('amount-left');
@@ -20,25 +24,88 @@ function enterAmount(){
   gamblingAmountText.innerHTML = gamblingAmount.value;
 }
 
+function bombClicked () {
+  squareOne.addEventListener("click", () => {
+    if (squareOne.value == "bomb") {
+      squareOne.style.background = "red";
+    }
+    else squareOne.style.background = "green";
+  })
+  squareTwo.addEventListener("click", () => {
+    if (squareTwo.value == "bomb") {
+      squareTwo.style.background = "red";
+    }
+    else squareTwo.style.background = "green";
+  })
+  squareThree.addEventListener("click", () => {
+    if (squareThree.value == "bomb") {
+      squareThree.style.background = "red";
+    }
+    else squareThree.style.background = "green";
+  })
+  squareFour.addEventListener("click", () => {
+    if (squareFour.value == "bomb") {
+      squareFour.style.background = "red";
+    }
+    else squareFour.style.background = "green";
+  })
+  squareFive.addEventListener("click", () => {
+    if (squareFive.value == "bomb") {
+      squareFive.style.background = "red";
+    }
+    else squareFive.style.background = "green";
+  })
+  squareSix.addEventListener("click", () => {
+    if (squareSix.value == "bomb") {
+      squareSix.style.background = "red";
+    }
+    else squareSix.style.background = "green";
+  })
+  squareSeven.addEventListener("click", () => {
+    if (squareSeven.value == "bomb") {
+      squareSeven.style.background = "red";
+    }
+    else squareSeven.style.background = "green";
+  })
+  squareEight.addEventListener("click", () => {
+    if (squareEight.value == "bomb") {
+      squareEight.style.background = "red";
+    }
+    else squareEight.style.background = "green";
+  })
+  squareNine.addEventListener("click", () => {
+    if (squareNine.value == "bomb") {
+      squareNine.style.background = "red";
+    }
+    else squareNine.style.background = "green";
+  })
+}
+
 function bombSquare() {
-  var random = Math.floor(Math.random() * 9) + 1;
-  console.log(random);
+  var random = Math.floor(Math.random() * 9);
+  for (let i = 0; i < 9; i++) {
+    if (i === random) {
+      array[i].value = "bomb";
+    }
+    if (array[i].value === "bomb") {
+      console.log("square " + i + " is the bomb square.")
+    }
+    bombClicked();
+  }
 }
 
 function newRound() {
-  var isBombSquare;
-  var random = Math.floor(Math.random() * 9) + 1;
-  for (let i = 1; i <= 9; i++) {
-    array[i] = i;
 
-  }
-  for (let i = 1; i <= 9; i++) {
-    if (array[i] == random) {
-      array[i] = "bombSquare";
-    }
-
-  }
+  bombSquare();
 
 }
 
-bombSquare();
+while (gamblingAmount.value > 0) {
+  newRound();
+}
+
+newRound();
+
+if (squareOne.clicked === true) {
+  console.log("clicked");
+}
